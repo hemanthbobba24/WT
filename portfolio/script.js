db42a37318f52050c9757e2454171e3b561cea5d
Hemanth Sriram <101417955+hemanthbobba24@users.noreply.github.com>
2022-10-28 15:22:45 +0530

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,7 +1,13 @@
 const list = document.querySelector('.list');
 const burgerBtn = document.querySelector('.burger-btn');
+const btn = document.querySelector('.btn');
 
 burgerBtn.addEventListener('click', () => {
   console.log('object');
   list.classList.toggle('show');
+});
+
+btn.addEventListener('click', () => {
+   window.location.href = "https://drive.google.com/file/d/1G5XqSQoDHKPxJebmMogPyHeaXJgOU3sg/view?usp=sharing";
+  
 });
